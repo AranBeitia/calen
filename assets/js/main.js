4bd9d777b788a6975f2e698bdee271a71a2444d0
AranBeitia <arantza.beitia@gmail.com>
2021-07-22 21:41:19 +0200

    start project

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,24 +7,52 @@ let today = new Date()
 let currentMonth = today.getMonth()
 let currentYear = today.getFullYear()
 
-let numberDaysMonth = new Date(currentYear, currentMonth, 0).getDate()
-let firstDay = new Date(currentYear, currentMonth, 1)
-let dayToString = firstDay.toLocaleDateString('en-GB', {
-  weekday: 'long'
-})
-
-let indexOfDay = arrayWeek.indexOf(dayToString)
-for (let i = 0; i <= (numberDaysMonth + indexOfDay); i++) {
-  let cellHTML = document.createElement('span')
-  if (i > indexOfDay) {
-    cellHTML.textContent = i - indexOfDay
-  }
-  console.log(cellHTML);
-  calendar.appendChild(cellHTML)
+function renderWeek() {
+  arrayWeek.forEach(day => {
+    let week = document.createElement('span')
+    week.textContent += day
+    calendar.appendChild(week)
+  })
 }
 
-console.log('current month ' + currentMonth);
-console.log('number days current month ' + numberDaysMonth);
-console.log(firstDay);
-console.log('the first day of the current month is: ' + dayToString);
-console.log(indexOfDay);
+function renderCalendar() {
+  renderWeek()
+  let numberDaysMonth = new Date(currentYear, currentMonth, 0).getDate()
+  let firstDay = new Date(currentYear, currentMonth, 1)
+  let dayToString = firstDay.toLocaleDateString('en-GB', {
+    weekday: 'long'
+  })
+
+  let indexOfDay = arrayWeek.indexOf(dayToString)
+  for (let i = 0; i <= (numberDaysMonth + indexOfDay); i++) {
+    let cellHTML = document.createElement('span')
+    if (i > indexOfDay) {
+      cellHTML.textContent = i - indexOfDay
+    }
+    calendar.appendChild(cellHTML)
+  }
+  // console.log('current month ' + currentMonth);
+  // console.log('number days current month ' + numberDaysMonth);
+  // console.log(firstDay);
+  // console.log('the first day of the current month is: ' + dayToString);
+  // console.log(indexOfDay);
+}
+
+function changeMonth() {
+  let previous = document.getElementById('previous')
+  let next = document.getElementById('next')
+  previous.addEventListener('click', () => {
+    calendar.innerHTML = ''
+    currentMonth--
+    renderCalendar()
+  })
+
+  next.addEventListener('click', () => {
+    calendar.innerHTML = ''
+    currentMonth++
+    renderCalendar()
+  })
+}
+
+renderCalendar()
+changeMonth()
